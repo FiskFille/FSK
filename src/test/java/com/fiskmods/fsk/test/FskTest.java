f13932ab671e3a625d7e9b1c6e1098eaa62743fa
FiskFille <fisk.randenback@gmail.com>
2023-02-13 19:37:01 +0100

    Switch over to using Gradle

--- src/test/java/com/fiskmods/fsk/test/FskTest.java
+++ src/test/java/com/fiskmods/fsk/test/FskTest.java
@@ -1,17 +1,19 @@
-package test;
+package com.fiskmods.fsk.test;
 
 import com.fiskmods.fsk.Compiler;
 import com.fiskmods.fsk.Script;
 import com.fiskmods.fsk.Var;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class FskTest
 {
     public static void main(String[] args) throws Exception
     {
-        String text = readFile(new File("test.fsk"));
+        String text = read(FskTest.class.getResourceAsStream("/test.fsk"));
         // Compile the text read from file into an FSK script object
         Script script = Compiler.compile(text);
 
@@ -33,18 +35,13 @@ public class FskTest
         System.out.println("Value of 'in' is: " + var.getAsDouble());
     }
 
-    public static String readFile(File file) throws IOException
+    public static String read(InputStream in) throws Exception
     {
-        if (file.exists() || file.createNewFile())
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in)))
         {
-            try (BufferedReader reader = new BufferedReader(new FileReader(file)))
-            {
-                String s = "";
-                for (String line; (line = reader.readLine()) != null; s += line + "\n");
-                return s;
-            }
+            String s = "";
+            for (String line; (line = reader.readLine()) != null; s += line + "\n");
+            return s;
         }
-
-        throw new FileNotFoundException(file.toString());
     }
 }
